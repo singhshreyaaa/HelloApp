a352f0685efbbf140aab2a7c5ca3fc2f65394c4d
singhshreyaaa <hs7762@srmist.edu.in>
2026-03-10 13:53:26 +0530

    dev branch

--- HelloApp.java
+++ HelloApp.java
@@ -1,8 +1,9 @@
+/*
+@author Shreya
+@versio 1.0
+*/
 class HelloApp{
-    public static void main (String[] args){
-	int num =100;
-	System.out.println(num);
-	System.out.println("Hello All");
-	
+	public static void main(String[] args){
+		System.out.println("This is dev branch");
 	}
-}	
+}
